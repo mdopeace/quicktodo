39a3c44a10e4579dc1d87de7527db11b909bd647
Md Mostafijur Rahman <md.correo.e@gmail.com>
2026-09-20 00:21:11 +0530

    revert: revert PR #25 (737048d) - revert to state before buggy release.sh changes

--- Formula/quicktodo.rb
+++ Formula/quicktodo.rb
@@ -1,8 +1,8 @@
 class Quicktodo < Formula
   desc "Minimal menu-bar todo app for macOS"
   homepage "https://github.com/mdopeace/quicktodo"
-  url "https://github.com/mdopeace/quicktodo/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "ed4a052fd2972f961c74326cc131025298e19c0b79ad1bd56d99dc8072c602cd"
+  url "https://github.com/mdopeace/quicktodo/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "PLACEHOLDER_FILLED_BY_RELEASE_SCRIPT"
 
   depends_on :macos
   depends_on :xcode => :build
@@ -17,7 +17,7 @@ class Quicktodo < Formula
       quicktodo.app installed to:
         #{opt_libexec}/quicktodo.app
 
-      To launch it:
+      To launch:
         open "#{opt_libexec}/quicktodo.app"
 
       To add to /Applications:
