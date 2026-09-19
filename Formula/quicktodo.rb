bcae46b299411effc0ea9cfb4118bb6382b9ef24
Md Mostafijur Rahman <md.correo.e@gmail.com>
2026-09-20 00:18:14 +0530

    fix: restore v1.1.1 with correct sha256 in Formula

--- Formula/quicktodo.rb
+++ Formula/quicktodo.rb
@@ -1,8 +1,8 @@
 class Quicktodo < Formula
   desc "Minimal menu-bar todo app for macOS"
   homepage "https://github.com/mdopeace/quicktodo"
-  url "https://github.com/mdopeace/quicktodo/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "PLACEHOLDER_FILLED_BY_RELEASE_SCRIPT"
+  url "https://github.com/mdopeace/quicktodo/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "ed4a052fd2972f961c74326cc131025298e19c0b79ad1bd56d99dc8072c602cd"
 
   depends_on :macos
   depends_on :xcode => :build
